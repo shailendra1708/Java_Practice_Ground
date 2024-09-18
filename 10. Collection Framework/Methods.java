3c4e734961523f3fbd5b7fc375bd6aab48a080c2
Shailendra Lodhi <shailendraverma1708@gmail.com>
2024-09-18 21:11:48 +0530

    Collection Interface Methods added

---
+++ 10. Collection Framework/Methods.java
@@ -0,0 +1,27 @@
+// add and addAll Method
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Methods {
+    public static void main(String[] args) {
+        // Create a collection (ArrayList)
+        Collection<String> fruits = new ArrayList<>();
+        
+        // Add individual elements using add()
+        fruits.add("Apple");
+        fruits.add("Banana");
+        
+        // Create another collection
+        Collection<String> moreFruits = new ArrayList<>();
+        moreFruits.add("Orange");
+        moreFruits.add("Mango");
+        
+        // Add all elements from moreFruits using addAll()
+        fruits.addAll(moreFruits);
+        
+        System.out.println("Fruits: " + fruits);
+    }
+}
+
+
